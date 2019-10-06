23616396785e045ea626c68013ea150685d03aa5
Cole Lawrence <cole@colelawrence.com>
2019-10-06 00:17:40 -0400

    Formatting

--- storybook/stories/writing/presentations/ObservableStateOct2019/TableOfContentsDeck.tsx
+++ storybook/stories/writing/presentations/ObservableStateOct2019/TableOfContentsDeck.tsx
@@ -6,35 +6,44 @@ import { Behavior } from "bloc-utils";
 import React, { useMemo } from "react";
 import { Deck, DeckProps } from "spectacle";
 
-
 export function ToCDeck(props: { children: React.ReactNode } & DeckProps) {
   const $tableOfContents = useMemo(
     () => new Behavior(ToCFromSlides(props.children)),
     [props.children]
   );
-  const showTableOfContents = !/export|presenter/.test(location.hash)
+  const showTableOfContents = !/export|presenter/.test(location.hash);
 
   return (
     <table style={{ position: "absolute", width: "100%", height: "100%" }}>
       <tr>
-        {showTableOfContents && 
-        <td style={{ width: "30%", fontSize: "1.5rem", padding: "8px" }}>
-          <$tableOfContents.react
-            next={sections =>
-              sections.map(section => (
-                <div key={section.sectionTitle || "un"}>
-                  {section.sectionTitle && <div style={{marginTop: '1rem'}}><b>{section.sectionTitle}</b></div>}
-                  {section.items.map(t => (
-                    <div style={{ color: t.active ? 'yellow' : 'unset', marginLeft: '1rem', display: 'inline-block' }}>
-                      {t.title}
-                    </div>
-                  ))}
-                </div>
-              ))
-            }
-          />
-        </td>
-        }
+        {showTableOfContents && (
+          <td style={{ width: "30%", fontSize: "1.5rem", padding: "8px" }}>
+            <$tableOfContents.react
+              next={sections =>
+                sections.map(section => (
+                  <div key={section.sectionTitle || "un"}>
+                    {section.sectionTitle && (
+                      <div style={{ marginTop: "1rem" }}>
+                        <b>{section.sectionTitle}</b>
+                      </div>
+                    )}
+                    {section.items.map(t => (
+                      <div
+                        style={{
+                          color: t.active ? "yellow" : "unset",
+                          marginLeft: "1rem",
+                          display: "inline-block"
+                        }}
+                      >
+                        {t.title}
+                      </div>
+                    ))}
+                  </div>
+                ))
+              }
+            />
+          </td>
+        )}
         <td style={{ position: "relative", height: "100%" }}>
           <Deck
             {...props}
