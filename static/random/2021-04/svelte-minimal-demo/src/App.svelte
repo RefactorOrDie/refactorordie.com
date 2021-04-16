c54e4c05813665e5695c8cbf4ed405f388a4f712
Cole Lawrence <cole@colelawrence.com>
2021-04-16 08:22:35 -0400

    Add /random stuff

---
+++ static/random/2021-04/svelte-minimal-demo/src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let name: string;
+	$: input1 = name
+</script>
+
+<main>
+	<h1>Hello {input1}!</h1>
+	<input type="text" bind:value={input1}>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
