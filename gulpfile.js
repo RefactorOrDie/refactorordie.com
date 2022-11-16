f1496cfa5672a69ec4a2f6e58c6157b77ec00b00
Cole Lawrence <cole@colelawrence.com>
2022-11-16 07:54:39 -0500

    Update builder for gulp-sass 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-sass.compiler = require('sass')
+// @ts-check
+"use strict";
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-const staticSrcPath = './static-src/'
-const staticPath = './themes/blank/static/'
-const assetsPath = './themes/blank/assets/'
+const staticSrcPath = "./static-src/";
+const staticPath = "./themes/blank/static/";
+const assetsPath = "./themes/blank/assets/";
 
-gulp.task('sass', () =>
-  gulp.src(staticSrcPath + 'style/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(assetsPath + 'style/'))
-)
+gulp.task("sass", () =>
+  gulp
+    .src(staticSrcPath + "style/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(assetsPath + "style/"))
+);
 
-gulp.task('watch', gulp.series('sass', () =>
-  gulp.watch(staticSrcPath + 'style/**/*.scss', ['sass'])
-))
+gulp.task(
+  "watch",
+  gulp.series("sass", function watchSass() {
+    return gulp.watch(staticSrcPath + "style/**/*.scss", gulp.task("sass"));
+  })
+);
 
-gulp.task('default', gulp.series('sass'))
+gulp.task("default", gulp.series("sass"));
