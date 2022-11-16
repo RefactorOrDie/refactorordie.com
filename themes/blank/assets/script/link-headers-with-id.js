883361efcec3d18663bf4548d3336ea671d66e01
Cole Lawrence <cole@colelawrence.com>
2022-11-16 13:50:48 -0500

    Fix flashing headers

--- themes/blank/assets/script/link-headers-with-id.js
+++ themes/blank/assets/script/link-headers-with-id.js
@@ -1,5 +1,6 @@
-;(function createLinksFromElementsWithId(document) {
-
+// @ts-check
+(function createLinksFromElementsWithId(document) {
+  /** @returns {Element} */
   function createElement(tag, text, attrs) {
     if (!attrs) attrs = {};
     if (text) attrs.innerHTML = text;
@@ -8,18 +9,28 @@
   }
 
   function prepend(el, elop) {
-    el.firstChild ? el.insertBefore(elop, el.firstChild) : el.appendChild(elop);
+    if (el.firstChild) el.insertBefore(elop, el.firstChild);
+    else el.appendChild(elop);
   }
 
-  function each(items, call) { Array.from(items).forEach(call) }
+  const linkStyle = ["color", "text-decoration", "position"]
+    .map((a) => a + ": inherit")
+    .join(";");
 
-  const linkStyle = ['color','text-decoration','position'].map(a => a + ': inherit').join(';');
-
-  each(document.querySelectorAll("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]"), el => {
-    let label = `Anchor link for: ${el.textContent}`;
-    let a = createElement("a", null, { href: `#${el.id}`, style: linkStyle, title: label });
+  for (const headingElt of document.querySelectorAll(
+    "h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]"
+  )) {
+    let label = `Anchor link for: ${headingElt.textContent}`;
+    let a = createElement("a", null, {
+      href: `#${headingElt.id}`,
+      style: linkStyle,
+      title: label,
+    });
     a.setAttribute("aria-label", label);
-    each(el.childNodes, elc => { elc.remove(); a.appendChild(elc) });
-    prepend(el, a);
-  });
+    for (const headingChildNode of headingElt.childNodes) {
+      headingElt.removeChild(headingChildNode)
+      a.appendChild(headingChildNode);
+    }
+    prepend(headingElt, a);
+  }
 })(document);
